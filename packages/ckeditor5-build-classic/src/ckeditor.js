c76a700b8d721b5e4afb4fd957035230c3239eaf
amine-meilleurescpi <amine@meilleurescpi.com>
2022-01-06 13:08:57 +0100

    customizing classic build: add mention

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -30,6 +30,8 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
+
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -58,7 +60,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Mention
 ];
 
 // Editor configuration.
